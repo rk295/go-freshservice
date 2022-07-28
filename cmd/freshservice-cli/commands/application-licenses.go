39278c67375522621d205b92b455c15fc971aab6
Robin Kearney <robin.kearney@qush.com>
2022-07-28 09:06:46 +0100

    two application related sub commands

---
+++ cmd/freshservice-cli/commands/application-licenses.go
@@ -0,0 +1,62 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	fs "github.com/rk295/go-freshservice/freshservice"
+	"github.com/spf13/cobra"
+)
+
+var (
+	applicationLicensesCmd = &cobra.Command{
+		Use:   "license",
+		Short: "List application licenses",
+		Run:   applicationLicenseRun,
+	}
+)
+
+func init() {
+	applicationLicensesCmd.Flags().Int64VarP(&appID, "application", "a", 0, "Get licenses for a specific application")
+	applicationCmd.AddCommand(applicationLicensesCmd)
+}
+
+func applicationLicenseRun(cmd *cobra.Command, args []string) {
+
+	if appID == 0 {
+		fmt.Println("Error: application id is required")
+		os.Exit(1)
+	}
+
+	token, domain, err := getConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	api, err := fs.New(ctx, domain, token, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var output interface{}
+
+	if appID != 0 {
+		t, err := api.Applications().ListLicenses(ctx, appID)
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		output = t
+	}
+
+	txt, err := printJSON(output)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(txt)
+}
--- cmd/freshservice-cli/commands/application.go
+++ cmd/freshservice-cli/commands/application.go
@@ -10,33 +10,70 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var applicationCmd = &cobra.Command{
-	Use:   "application",
-	Short: "List applications",
-	Run:   applicationRun,
-}
+var (
+	applicationCmd = &cobra.Command{
+		Use:   "application",
+		Short: "List applications",
+		Run:   applicationRun,
+	}
+
+	appID int64
+)
 
 func init() {
+	applicationCmd.Flags().Int64VarP(&appID, "application", "a", 0, "Get a specific application")
 	rootCmd.AddCommand(applicationCmd)
 }
 
 func applicationRun(cmd *cobra.Command, args []string) {
-	token := os.Getenv("TOKEN")
-	domain := os.Getenv("DOMAIN")
+	token, domain, err := getConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	ctx := context.Background()
+
 	api, err := fs.New(ctx, domain, token, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	t, _, err := api.Applications().List(ctx)
-	if err != nil {
-		log.Fatal("Error: ", err)
+	var output interface{}
+
+	if appID != 0 {
+		t, err := api.Applications().Get(ctx, appID)
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		output = t
+	} else {
+		page := "page=1"
+		applications := &fs.Applications{}
+
+		for {
+			t, nextPage, err := api.Applications().List(ctx, &fs.ApplicationListOptions{PageQuery: page})
+			if err != nil {
+				log.Println(t)
+				log.Fatal("Error: ", err)
+			}
+
+			applications.List = append(applications.List, t...)
+
+			if nextPage == "" {
+				break
+			}
+
+			page = nextPage
+		}
+
+		output = applications
 	}
 
-	fmt.Print("All Applications:\n")
-	for _, app := range t {
-		fmt.Printf("%d - %s\n", app.ID, app.Name)
+	txt, err := printJSON(output)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Println(txt)
 }
