dfcece4ba1d9cd457db5d655c18ebf12aac88f7d
Robin Kearney <robin.kearney@qush.com>
2022-08-10 22:08:02 +0100

    Helper constant

---
+++ freshservice/constants.go
@@ -0,0 +1,13 @@
+package freshservice
+
+const (
+	// FreshserviceAPITokenEnvName is a convenience constant that can be used
+	// as an environment variable name for configuring the client with an API
+	// token.
+	FreshserviceAPITokenEnvName = "FRESHSERVICE_API_TOKEN"
+
+	// FreshserviceCompanyDomainEnvName is a convenience constant that can be
+	// used as an environment variable name for configuring the client with a
+	// Freshservice domain.
+	FreshserviceCompanyDomainEnvName = "FRESHSERVICE_COMPANY_DOMAIN"
+)
